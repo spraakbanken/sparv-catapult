110b19511f1a4bfe7549d4290899326e08c4647f
Anne Schumacher <anne.schumacher@gu.se>
2016-04-12 13:37:27 +0000

    added Makefile.langs

--- setup-server.sh
+++ setup-server.sh
@@ -2,5 +2,6 @@
 
 svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.rules --force
 svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.config --force
+svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.langs --force
 
 make
