7733c13ad5ef9259c4d0418ef21260c4d91b686d
Anne Schumacher <anne.schumacher@gu.se>
2018-05-24 16:34:36 +0200

    change catapult dir

--- config.sh
+++ config.sh
@@ -12,10 +12,9 @@ SPARV_BIN="${PIPELINE}/bin"
 SPARV_MAKEFILES="${PIPELINE}/makefiles"
 
 # catapult, builds and log directory
-CATAPULT_DIR="${BACKEND}/data/catapult"
+CATAPULT_DIR="${BACKEND}/catapult"
 BUILDS_DIR="${BACKEND}/data/builds"
 LOGDIR="${BACKEND}/logs"
 
 # Path to virtualenv, optional
 CATAPULT_VENV="${CATAPULT_DIR}/venv"
-
